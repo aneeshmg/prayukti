2818788ee339d44bd16607c70d66d249b9e9d9ea
Aneesh Gangadhar <aneesh.mg2007@gmail.com>
2020-03-02 17:39:27 -0700

    trianglyfy ui

--- src/siteHandlers.js
+++ src/siteHandlers.js
@@ -1,8 +1,14 @@
-const db = require('./db')
+const _db = require('./db')
 
 const index = (req, res) => {
+    let db = _db.getDb()
+    
+    let data = db[Math.floor(Math.random() * db.length)]
+
     res.render('index', {
         title: 'Prayukti | Get motivated!',
+        quote: data.quote,
+        source: data.source == "" ? "Unknown" : data.source
     })
 }
 
--- src/views/js/main.js
+++ src/views/js/main.js
@@ -1,1 +1,8 @@
-console.log("Check check..")
+let pattern = Trianglify({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    variance: Math.random(),
+    cell_size: Math.floor(Math.random() * (200 - 50 + 1) + 50)
+})
+
+document.body.background = pattern.png()
